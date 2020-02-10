245a1069e336437e7a43b0b05dd9d5249de597f3
Austin <Nazanator@hotmail.com>
2020-02-10 17:21:44 +0000

    Add basic login form with no functionality, add another resize breakpoint

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -2,10 +2,25 @@ function nav() {
   $('.nav-menu').on('click', function() {
     $('.menu').slideToggle();
   })
+
+  $('.auth').on('click', function() {
+    $(this).slideToggle();
+  })
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
 
 $( document ).ready(function() {
   nav();
+  openForm();
+  closeForm();
+  // $('.auth').hide();
 
   // Had to force the menu to show or hide on resize
   $(window).on('resize', function() {
